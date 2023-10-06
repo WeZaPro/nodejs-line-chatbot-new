e04beecad5b7a6507c69ad8e0f6f2ab875666caf
WeZaPro <taweesak9359@gmail.com>
2023-10-06 19:43:50 +0700

    update3

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(
 // });
 
 app.post("/webhook", function (req, res) {
+  console.log("res.body.event ==> ", res.body.event);
   res.send("HTTP POST request sent to the webhook URL!");
   // If the user sends a message to your bot, send a reply message
   //   if (req.body.events[0].type === "message") {
